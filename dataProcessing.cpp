22a8a0dd1ec2e3862201eec7e471f9890b8080a1
Maurcio Pedro Vieira <mauriciopsgv@gmail.com>
2017-09-25 18:43:27 -0300

    Avanços no algoritmo em C

--- dataProcessing.cpp
+++ dataProcessing.cpp
@@ -3,8 +3,9 @@
 #include <string>
 #include <vector>
 #include <chrono>
+#include <random>
 
-double EuclideanDistanceSquared( std::vector<int> v1, std::vector<int> v2)
+double EuclideanDistanceSquaredVectors( std::vector<int> v1, std::vector<int> v2)
 {
 	if (v1.size() != v2.size())
 		return -1;
@@ -17,6 +18,118 @@ double EuclideanDistanceSquared( std::vector<int> v1, std::vector<int> v2)
 	return sum;
 }
 
+double EuclideanDistanceSquaredInt(int * v1, int * v2, int dimension)
+{
+
+	double sum = 0;
+	for (int i = 0; i < dimension; i++)
+	{
+		sum += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+	}
+	return sum;
+}
+
+
+double EuclideanDistanceSquaredDouble(double * v1, double* v2, int dimension)
+{
+	double sum = 0;
+	for (int i = 0; i < dimension; i++)
+	{
+		sum += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+	}
+	return sum;
+}
+
+
+double ** randomGaussianMatrix(int numDocs, int dimension)
+{
+	double ** matrix = new double*[numDocs];
+	for (int i = 0; i < numDocs; i++)
+	{
+		matrix[i] = new double[dimension];
+	}
+
+	std::default_random_engine generator;
+	std::normal_distribution<double> distribution(0.0, sqrt(1.0 / numDocs));
+
+	for (int i = 0; i < numDocs; i++)
+	{
+		for (int j = 0; j < dimension; j++)
+		{
+			matrix[i][j] = distribution(generator);
+		}
+	}
+	return matrix;
+}
+
+double ** gaussianProjection(std::vector<std::vector<int>> documents, double** Wgaussian, int numDocs, int dimension)
+{
+	int i, j, k;
+	double ** matrix = new double*[numDocs];
+	for (int i = 0; i < numDocs; i++)
+	{
+		matrix[i] = new double[dimension];
+	}
+
+	for (int i = 0; i < numDocs; i++)
+	{
+		for (int j = 0; j < dimension; j++)
+		{
+			double soma = 0;
+			for (int k = 0; k < dimension; k++)
+			{
+				soma += documents[i][k] * Wgaussian[k][j];
+			}
+			matrix[i][j] = soma;
+		}
+	}
+	return matrix;
+}
+
+int ** randomAchiloptasMatrix(int numDocs, int dimension)
+{
+	int ** matrix = new int*[numDocs];
+	for (int i = 0; i < numDocs; i++)
+	{
+		matrix[i] = new int[dimension];
+	}
+
+	int possibleValues[6] = { 1,0,0,0,0,-1 };
+
+	for (int i = 0; i < numDocs; i++)
+	{
+		for (int j = 0; j < dimension; j++)
+		{
+			matrix[i][j] = possibleValues[rand() % 6];
+		}
+	}
+	return matrix;
+}
+
+int ** achiloptasProjection(std::vector<std::vector<int>> documents, int** Wachiloptas, int numDocs, int dimension)
+{
+	int i, j, k;
+	int ** matrix = new int*[numDocs];
+	for (int i = 0; i < numDocs; i++)
+	{
+		matrix[i] = new int[dimension];
+	}
+
+	for (int i = 0; i < numDocs; i++)
+	{
+		for (int j = 0; j < dimension; j++)
+		{
+			double soma = 0;
+			for (int k = 0; k < dimension; k++)
+			{
+				soma += documents[i][k] * Wachiloptas[k][j];
+			}
+			matrix[i][j] = soma;
+		}
+	}
+	return matrix;
+}
+
 int main (void)
 {	
 	// For more precise clock
@@ -25,6 +138,19 @@ int main (void)
 	int numDocs, sizeDocs;
 	std::vector<std::string> tokens;
 	std::vector<std::vector<int>> documents;
+
+	// testing achloptas matrix
+	int ** test = randomAchiloptasMatrix(20, 20);
+
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 20; j++)
+		{
+			std::cout << test[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0 ;
 
 	// Loading Documents
 
@@ -78,7 +204,7 @@ int main (void)
 		distances[i][i] = 0.0;
 		for (int j = i + 1; j < numDocs; j++)
 		{
-			distances[j][i] = EuclideanDistanceSquared(documents[i], documents[j]);
+			distances[j][i] = EuclideanDistanceSquaredVectors(documents[i], documents[j]);
 		}
 	}
 
@@ -97,6 +223,24 @@ int main (void)
 		}
 		std::cout << std::endl;
 	}
+
+	// main loop
+
+	start = clock::now();
+	int ** Wachiloptas = randomAchiloptasMatrix(numDocs, sizeDocs);
+	std::chrono::duration<double> timeToCalculateAchiloptasMatrix = clock::now() - start;
+
+	start = clock::now();
+	double ** Wgaussian = randomGaussianMatrix(numDocs, sizeDocs);
+	std::chrono::duration<double> timeToCalculateGaussianMatrix = clock::now() - start;
+
+	start = clock::now();
+	int ** projectedAchiloptas = achiloptasProjection(documents, Wachiloptas, numDocs, sizeDocs);
+	std::chrono::duration<double> projectionTimeGaussian = clock::now() - start;
+
+	start = clock::now();
+	double ** projectedGaussian = gaussianProjection(documents, Wgaussian, numDocs, sizeDocs);
+	std::chrono::duration<double> projectionTimeGaussian = clock::now() - start;
 
 	return 0;
 }
